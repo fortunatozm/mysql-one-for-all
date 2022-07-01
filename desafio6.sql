d87596bc833eaed508a7a30a2792170c627dafe1
fortunatozm <fortunatompongo@hotmail.com>
2022-07-01 07:24:58 -0300

    Desafio 6 - Corrigido

--- desafio6.sql
+++ desafio6.sql
@@ -1,3 +1,3 @@
-select min(plano.valor_plano) as faturamento_minimo, max(plano.valor_plano) as faturamento_maximo, round(avg(plano.valor_plano), 2) as faturamento_medio, sum(plano.valor_plano) as faturamento_total
+select convert(round(min(plano.valor_plano), 2), nchar) as faturamento_minimo, convert(round(max(plano.valor_plano), 2), nchar) as faturamento_maximo, convert(round(avg(plano.valor_plano), 2), nchar) as faturamento_medio, convert(round(sum(plano.valor_plano), 2), nchar) as faturamento_total
 	from SpotifyClone.plano
     inner join SpotifyClone.users on plano.id = users.plano_id
